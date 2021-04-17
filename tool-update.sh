1eb89b2bd624594128fb396848a2c26fa73c5f88
A l - A l a m y <59588007+AL-AlamySploit@users.noreply.github.com>
2021-04-17 22:42:53 +0200

    Update tool-update.sh

--- tool-update.sh
+++ tool-update.sh
@@ -2,6 +2,8 @@ A='\033[01;32m'
 pkg install python
 pkg install git
 cd
-rm -rif termux-packes
+cd
+mv termux-packages termux-packages2
+rm -rif termux-packages2
 git clone https://github.com/AL-AlamySploit/termux-packages
 echo "$A[+] Done..!"
